85345f29a54b8c8510f5c09094084b9341cbc8e1
Aman Rao Sanas <61731654+AmanRaoSanas@users.noreply.github.com>
2022-01-27 23:41:11 +0530

    Update settings.py
    Deployment changes done

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gb$+habwg5^tbt1*_t_ng%&jv&jf)httr=_23&&ztspsc+u)^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
